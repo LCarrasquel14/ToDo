3393ee6c89fbfaed3605393ef64d4a064ccd97f8
Lcarrasquel14 <ldgarcia2804@gmail.com>
2024-06-21 10:50:52 -0400

    button component added to component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/ui/Button/Button";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Button label="New template" primary={true} size="medium" />
+    </div>
+  );
 };
 
 export default page;
